49378c233b1ae84085e91382d216a9f73722448b
Joao Lopes <jgsantanalopes@gmail.com>
2025-03-05 12:22:20 -0300

    encapsulating commands

--- cypress/e2e/tracking.cy.js
+++ cypress/e2e/tracking.cy.js
@@ -1,42 +1,46 @@
-describe('Consulta de Encomenda no Chat', () => {
-  it('Deve indicar que a encomenda já foi entregue', () => {
+describe('Order inquiry via chatbot', () => {
+  it('It should show the order as delivered', () => {
 
     const trackingCode = 'PD123456785BR'
-    cy.viewport('iphone-xr')
-    cy.visit('/')
-    cy.get('button[aria-label="Open Chat"]')
-      .should('be.visible')
-      .click()
 
-    cy.contains('.rcb-bot-message', 'Olá! Tudo bem? Posso te ajudar a consultar o status da sua encomenda?')
-      .should('be.visible')
-
-    cy.contains('.rcb-options', 'Sim, por favor!')
-      .click()
-
-    cy.contains('.rcb-bot-message', 'Ótimo! Por favor, digite o código de rastreio da sua encomenda:')
-      .should('be.visible')
-
-    cy.get('textarea[placeholder^="Escreva sua mensagem"]')
-      .type(trackingCode)
-
-    cy.get('.rcb-send-button')
-      .click()
-
-    cy.contains('.rcb-bot-message', `Confirmando: você informou o código de rastreio ${trackingCode}. Está tudo certo?`)
-      .should('be.visible')
-
-    cy.contains('.rcb-options', 'Sim, está certo!')
-      .click()
-
-    cy.contains('.rcb-bot-message', 'Perfeito! Estou consultando as informações nos Correios... Só um instante. 📦🔍')
-      .should('be.visible')
-
-    cy.contains(
-      '.rcb-bot-message', 
-      'Boa notícia! Sua encomenda já foi entregue com sucesso. 🎉 Se precisar de algo mais, é só me chamar!',
-      {timeout: 7000}
-    )
-      .should('be.visible')
+    cy.openChatBot()
+    cy.checkMessage('Olá! Tudo bem? Posso te ajudar a consultar o status da sua encomenda?')
+    cy.selectOption('Sim, por favor!')
+    cy.checkMessage('Ótimo! Por favor, digite o código de rastreio da sua encomenda:')
+    cy.sendMessage(trackingCode)
+    cy.checkMessage(`Confirmando: você informou o código de rastreio ${trackingCode}. Está tudo certo?`)
+    cy.selectOption('Sim, está certo!')
+    cy.checkMessage('Perfeito! Estou consultando as informações nos Correios... Só um instante. 📦🔍')
+    cy.checkMessage('Boa notícia! Sua encomenda já foi entregue com sucesso. 🎉 Se precisar de algo mais, é só me chamar!', 7000)
   })
 })
+
+Cypress.Commands.add('openChatBot', () => {
+  cy.viewport('iphone-xr')
+  cy.visit('/')
+  cy.get('button[aria-label="Open Chat"]')
+    .should('be.visible')
+    .click()
+
+  cy.get('.rcb-chat-header span')
+    .should('be.visible')
+    .and('have.text', 'Sensei')
+})
+
+Cypress.Commands.add('checkMessage', (expectMessage, timeout = 4000) => {
+  cy.contains('.rcb-bot-message', expectMessage, { timeout: timeout })
+    .should('be.visible')
+})
+
+Cypress.Commands.add('selectOption', (option) => {
+  cy.contains('.rcb-options', option)
+    .click()
+})
+
+Cypress.Commands.add('sendMessage', (message) => {
+  cy.get('textarea[placeholder^="Escreva sua mensagem"]')
+    .type(message)
+
+  cy.get('.rcb-send-button')
+    .click()
+})  
