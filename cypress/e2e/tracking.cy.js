98a91514e3c6ee16ccaf5035b34874f19f343529
Joao Lopes <jgsantanalopes@gmail.com>
2025-03-05 14:22:22 -0300

    refactor to remove duplicate code

--- cypress/e2e/tracking.cy.js
+++ cypress/e2e/tracking.cy.js
@@ -1,46 +1,24 @@
 describe('Order inquiry via chatbot', () => {
-  it('It should inform that the order as delivered', () => {
 
-    const trackingCode = 'PD123456785BR'
+  const scenarios = [
+    {title: 'It should inform that the order as delivered', trackingCode: 'PD123456785BR', finalMessage: 'Boa notícia! Sua encomenda já foi entregue com sucesso. 🎉 Se precisar de algo mais, é só me chamar!'},
+    {title: 'It should inform that the order is on the way', trackingCode: 'BR987654321BR', finalMessage: 'A sua encomenda já foi despachada e está a caminho! 🚚 Prazo estimado: 5 dias úteis.'},
+    {title: 'It should inform that the order is out for delivery', trackingCode: 'QW112233445BR', finalMessage: 'Ótima notícia! Sua encomenda está em rota de entrega e chega ainda hoje. Fique de olho! 👀📦'}
+  ]
 
-    cy.openChatBot()
-    cy.checkMessage('Olá! Tudo bem? Posso te ajudar a consultar o status da sua encomenda?')
-    cy.selectOption('Sim, por favor!')
-    cy.checkMessage('Ótimo! Por favor, digite o código de rastreio da sua encomenda:')
-    cy.sendMessage(trackingCode)
-    cy.checkMessage(`Confirmando: você informou o código de rastreio ${trackingCode}. Está tudo certo?`)
-    cy.selectOption('Sim, está certo!')
-    cy.checkMessage('Perfeito! Estou consultando as informações nos Correios... Só um instante. 📦🔍')
-    cy.checkMessage('Boa notícia! Sua encomenda já foi entregue com sucesso. 🎉 Se precisar de algo mais, é só me chamar!', 7000)
-  })
-
-  it('It should inform that the order is on the way', () => {
-
-    const trackingCode = 'BR987654321BR'
-
-    cy.openChatBot()
-    cy.checkMessage('Olá! Tudo bem? Posso te ajudar a consultar o status da sua encomenda?')
-    cy.selectOption('Sim, por favor!')
-    cy.checkMessage('Ótimo! Por favor, digite o código de rastreio da sua encomenda:')
-    cy.sendMessage(trackingCode)
-    cy.checkMessage(`Confirmando: você informou o código de rastreio ${trackingCode}. Está tudo certo?`)
-    cy.selectOption('Sim, está certo!')
-    cy.checkMessage('Perfeito! Estou consultando as informações nos Correios... Só um instante. 📦🔍')
-    cy.checkMessage('A sua encomenda já foi despachada e está a caminho! 🚚 Prazo estimado: 5 dias úteis.', 7000)
-  })
-
-  it('It should inform that the order is out for delivery', () => {
-    const trackingCode = 'QW112233445BR'
-
-    cy.openChatBot()
-    cy.checkMessage('Olá! Tudo bem? Posso te ajudar a consultar o status da sua encomenda?')
-    cy.selectOption('Sim, por favor!')
-    cy.checkMessage('Ótimo! Por favor, digite o código de rastreio da sua encomenda:')
-    cy.sendMessage(trackingCode)
-    cy.checkMessage(`Confirmando: você informou o código de rastreio ${trackingCode}. Está tudo certo?`)
-    cy.selectOption('Sim, está certo!')
-    cy.checkMessage('Perfeito! Estou consultando as informações nos Correios... Só um instante. 📦🔍')
-    cy.checkMessage('Ótima notícia! Sua encomenda está em rota de entrega e chega ainda hoje. Fique de olho! 👀📦', 7000)
+  scenarios.forEach((scenario) => {
+    it(scenario.title, () => {
+  
+      cy.openChatBot()
+      cy.checkMessage('Olá! Tudo bem? Posso te ajudar a consultar o status da sua encomenda?')
+      cy.selectOption('Sim, por favor!')
+      cy.checkMessage('Ótimo! Por favor, digite o código de rastreio da sua encomenda:')
+      cy.sendMessage(scenario.trackingCode)
+      cy.checkMessage(`Confirmando: você informou o código de rastreio ${scenario.trackingCode}. Está tudo certo?`)
+      cy.selectOption('Sim, está certo!')
+      cy.checkMessage('Perfeito! Estou consultando as informações nos Correios... Só um instante. 📦🔍')
+      cy.checkMessage(scenario.finalMessage, 7000)
+    })
   })
 
   it('It should display an error for tracking code not found', () => {
@@ -54,7 +32,7 @@ describe('Order inquiry via chatbot', () => {
     cy.checkMessage(`Confirmando: você informou o código de rastreio ${trackingCode}. Está tudo certo?`)
     cy.selectOption('Sim, está certo!')
     cy.checkMessage('Perfeito! Estou consultando as informações nos Correios... Só um instante. 📦🔍')
-    cy.checkMessage('Hmm... Não encontrei uma encomenda com os dados informados. Vamos tentar de novo? 👀📦', 7000)
+    cy.checkMessage('Hmm... Não encontrei uma encomenda com os dados informados. Vamos tentar de novo?', 7000)
     cy.selectOption('Encerrar atendimento')
     cy.checkMessage('Obrigado por falar comigo! 😊 Se precisar de mais alguma coisa, é só chamar.')
   })
